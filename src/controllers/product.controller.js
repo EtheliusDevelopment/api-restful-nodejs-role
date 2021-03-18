12476fdb6f830ceb77ffcd1ab596548a07634e1d
EtheliusDevelopment <etheliusdev@gmail.com>
2021-03-18 13:47:15 +0100

    api products working

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,25 +2,33 @@ import Product from '../models/Product'
 
 export const createProduct = async (req, res) => {
     
-    const { name, category, price, imageURL} = req.body
-    const newProduct = new Product ({name, category, price, imageURL})
-    await newProduct.save();
+    const { name, category, price, imgeURL} = req.body
+    const newProduct = new Product ({name, category, price, imgeURL})
+    const productSaved = await newProduct.save();
+
     
-    res.json('creating product')
+    res.status(201).json(productSaved)
 };
 
-export const getProducts = (req, res) => {
-    res.json("get products")
+export const getProducts = async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
 };
 
-export const getProductById = (req, res) => {
-
+export const getProductById = async (req, res) => {
+    const product = await Product.findById(req.params.productId);
+    res.status(200).json(product)
 };
 
-export const updateProductById = (req, res) => {
-
+export const updateProductById = async (req, res) => {
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.productId, req.body,{
+        new : true
+    })
+    res.status(200).json(updatedProduct)
 };
 
-export const deleteProductById = (req, res) => {
+export const deleteProductById = async (req, res) => {
+    await Product.findByIdAndDelete(req.params.productId)
+    res.status(200).json('Product Deleted')
 
 };
